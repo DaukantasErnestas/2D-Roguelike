c04d3519f562ffc4ebe012dfab94233eef7a0802
DaukantasErnestas <daukantasernestas33@gmail.com>
2022-02-07 16:26:19 +0200

    Added enemy spawn sound
    I realised some sound was missing for the enemies so i added a sound for when they spawn

--- PreEnemySpawner.gd
+++ PreEnemySpawner.gd
@@ -2,6 +2,7 @@ extends Particles2D
 
 export(PackedScene) var enemy_to_spawn
 onready var enemy_spawn_particles = preload("res://Particles/EnemySpawnParticles.tscn")
+onready var enemy_spawn_sound = preload("res://SoundObjects/Enemy_Spawn.tscn")
 var enemy
 
 func _ready():
@@ -12,6 +13,8 @@ func _ready():
 func _on_SpawnTimer_timeout():
 	emitting = false
 	var particles_instance = enemy_spawn_particles.instance()
+	var sound_instance = enemy_spawn_sound.instance()
+	Global.PlaySound(enemy_spawn_sound,global_position,Global.node_creation_parent)
 	particles_instance.global_position = global_position
 	particles_instance.modulate = enemy.get_node("Sprite").modulate
 	Global.node_creation_parent.add_child(enemy)
